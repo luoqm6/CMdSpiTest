e525e5e8e7b976e30cbcf237afdd7be3cb1c910b
luoqingming <390571270@qq.com>
2018-03-06 16:20:34 +0800

    add argv,need config.ini

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,15 @@
 #include <string>
 #include <cstring>
 #include <dlfcn.h>
+#include <cstdlib>
+#include <unistd.h>
 #include "ThostFtdcMdApi.h"
 #include "ThostFtdcUserApiDataType.h"
 #include "ThostFtdcTraderApi.h"
 #include "ThostFtdcUserApiStruct.h"
 #include "CMdSpi.h"
 #include "CTraderSpi.h"
+#include "Ini.h"
 
 using namespace std;
 
@@ -23,28 +26,82 @@ char gMdFrontAddr[] = "tcp://180.168.146.187:10010";
 TThostFtdcBrokerIDType gBrokerID = "9999";		//code of the broker
 TThostFtdcInvestorIDType gInvestorID = "112573";
 TThostFtdcPasswordType gInvestorPassword = "Luoqingming1997";
-char *ppInstrumentID[] = {"cu1805","cu1806"};
+char *ppInstrumentID[] = {};
 int instrumentNum = 2;
 
 //request id
 int iRequestID = 0;
 
-int main()
+int main(int argc,char* argv[])
 {
-	// cout<<"Enter investerID: ";
-	// scanf("%s",gInvestorID);
-	// cout<<"Enter password: ";
-	// scanf("%s",gInvestorPassword);
+
+	printf("-------------\n");
+
+	if(argc < 2)
+	{
+		printf("Usage: ./CTP -options\n");
+		printf("Use \"./CTP\" -h to see the options\n");
+		exit(0);
+	}
+	char ch;
+
+	char* filePath;
+
+	while((ch = getopt(argc, argv, "b:i:p:f:n:h"))!= -1){
+		switch(ch){
+
+			case 'b':
+				sprintf(gBrokerID,"%s",optarg);
+				printf("%s\n", gBrokerID);
+				break;
+
+			case 'i':
+				sprintf(gInvestorID,"%s",optarg);
+				printf("%s\n", gInvestorID);
+				break;
+
+			case 'p':
+				sprintf(gInvestorPassword,"%s",optarg);
+				printf("%s\n", gInvestorPassword);
+				break;
+
+			case 'f':
+				filePath = optarg;
+				printf("%s\n", filePath);
+				break;
+
+			case 'n':
+				instrumentNum = atoi(optarg);
+				printf("%d\n", instrumentNum);
+				break;
+
+			case 'h':
+				printf("Options:\n");
+				printf("Usage: ./CTP -opt1 para1 -opt2 para2 ...\n");
+				printf("-b: Broker ID\n");
+				printf("-i: Investor ID\n");
+				printf("-p: Investor Password\n");
+				printf("-f: config filepath\n");
+				printf("-n: Sum of instrument\n");
+				printf("-h: Help to list the options\n");
+				break;
+
+			default:
+				printf("Usage: ./CTP -opt1 para1 -opt2 para2 ...\n");
+				printf("Use \"./CTP -h\" to see the options\n");
+				exit(0);
+				break;
+		}
+	}
     
-    // CMdSpi
+	// CMdSpi
+	// pMdUserApi = CThostFtdcMdApi::CreateFtdcMdApi();
+	// CMdSpi* pMdUserSpi = new CMdSpi();
+	// pMdUserApi->RegisterSpi(pMdUserSpi);
+	// pMdUserApi->RegisterFront(gMdFrontAddr);
+	// pMdUserApi->Init();
 
-	pMdUserApi = CThostFtdcMdApi::CreateFtdcMdApi();
-	CMdSpi* pMdUserSpi = new CMdSpi();
-	pMdUserApi->RegisterSpi(pMdUserSpi);
-	pMdUserApi->RegisterFront(gMdFrontAddr);
-	pMdUserApi->Init();
-
-    pMdUserApi->Join();
+	// pMdUserApi->Join();
 
 	//pMdUserApi->Release();
 	return 0;
